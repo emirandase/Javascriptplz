6dec650753f4b2c64ad7cbddf41c0f8a6ff1b0b6
emirandase <emirandase@gmail.com>
2020-09-07 21:08:17 -0300

    se modifica el archivo .js

---
+++ Ejercicio_script.js
@@ -0,0 +1,53 @@
+//T1 edad aproximada
+const edadAproximada = (añoActual, añoNacimiento) => {
+    return console.log (añoActual - añoNacimiento);
+}
+
+edadAproximada(2020, 1991);
+
+/*
+//T2 calculo de edades
+
+const niño = (añoActual, añoNacimiento) => {
+    return console.log (añoActual - añoNacimiento);
+}
+
+const adolescente = (añoActual, añoNacimiento) => {
+    return console.log (añoActual - añoNacimiento);
+}
+
+const adulto = (añoActual, añoNacimiento) => {
+    return console.log (añoActual - añoNacimiento);
+}
+
+const terceraEdad = (añoActual, añoNacimiento) => {
+    return console.log (añoActual - añoNacimiento);
+}
+
+niño(2020,2010);
+adolescente(2020,2000);
+adulto(2020,1970);
+terceraEdad(2020,1969);
+
+//FIN T2
+
+//T3 Numeros Positivos y Negativos
+
+const positivo = (valorUno, valorDos) => {
+    return console.log (valorUno / valorDos);
+}
+
+const negativo = (valorUno, valorDos) => {
+    return console.log (valorUno / valorDos);
+}
+
+const neutro = (valorUno, valorDos) => {
+    return console.log (valorUno / valorDos);
+}
+
+positivo(4,2);
+negativo(-8,6);
+neutro(0,2);
+
+//FIN T3
+*/
